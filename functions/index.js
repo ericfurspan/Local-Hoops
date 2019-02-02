b218dc18566b4321df3b33c50fd5830bec22471a
Eric Furspan <eric.furspan@gmail.com>
2019-02-02 13:57:57 -0500

    created firestore trigger

--- functions/index.js
+++ functions/index.js
@@ -52,7 +52,7 @@ exports.openFriendRequest = functions.firestore.document('friendRequests/{reques
         }
         // send notification
         await admin.messaging().send(notification);
-        console.log(`Successfully sent notification to ${data.requesteeId}`);
+        console.log(`Successfully sent notification to ${notificationRecipientId}`);
       }
     } else {
       return console.error(`Something went wrong. No data found for requestId: ${requestId}`);
@@ -60,8 +60,7 @@ exports.openFriendRequest = functions.firestore.document('friendRequests/{reques
   } catch(e) {
     console.error(e);
   }
-  
-})
+});
 
 // handles closing friend requests
 // sends notification and deletes friend request document
@@ -121,11 +120,63 @@ exports.closeFriendRequest = functions.firestore.document('friendRequests/{reque
         }
         // send notification
         await admin.messaging().send(notification);
-        console.log(`Successfully sent notification to ${afterData.requestorId}`);
+        console.log(`Successfully sent notification to ${notificationRecipientId}`);
       }
     } else {
         return console.error(`Something went wrong. No data found for requestId: ${requestId}`);
     }
+  } catch(e) {
+    console.error(e);
+  }
+});
+
+exports.newEventNotification = functions.firestore.document('events/{eventId}')
+.onCreate(async (snapshot) => {
+
+  try {
+    const data = snapshot.data();
+    const participants = data.participants.filter(uid => uid !== data.event_author);
+
+    // get name of event_author
+    const nameSnapshot = await admin.firestore().doc(`users/${data.event_author}`).get();
+    const authorName = nameSnapshot.data().displayName;
+
+    for(let i=0;i<participants.length;i++) {
+
+      const notificationRecipientId = participants[i];
+      const title = `New ${data.type}`;
+      const body = `${authorName} tagged you in an event`;
+  
+      // get fcmToken for notification recipient
+      const tokenSnapshot = await admin.firestore().doc(`users/${notificationRecipientId}`).get();
+      const fcmToken = tokenSnapshot.data().fcmToken;
+
+      // send notification
+      if(fcmToken) {
+        // build the notification
+        const notification = {
+          "notification": {
+            "title": title,
+            "body": body,
+          },
+          "apns": {
+            "payload": {
+              "aps": {
+                "content-available": 1,
+                "alert": {
+                  "body": body,
+                },
+                "badge": 0                      
+              }
+            }
+          },
+          "token": fcmToken
+        }
+        // send notification
+        await admin.messaging().send(notification);
+        console.log(`Successfully sent notification to ${notificationRecipientId}`);
+      }  
+    } 
   } catch(e) {
     console.error(e);
   }
