2e113404adc6521e9cbc56ea2a69bb6c821e1e53
Eric Furspan <eric.furspan@gmail.com>
2019-03-04 10:07:05 -0500

    Trim cloud fns

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,6 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-// const helper = require('./helper');
+// const util = require('./util');
 
 admin.initializeApp();
 
