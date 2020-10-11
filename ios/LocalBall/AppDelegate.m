d4ece5ce77a5a592618c0443c305169087a150af
Eric Furspan <eric.furspan@gmail.com>
2020-10-11 19:57:36 -0400

    Update ios/ files

--- ios/LocalBall/AppDelegate.m
+++ ios/LocalBall/AppDelegate.m
@@ -14,10 +14,24 @@
 #import <RNGoogleSignin/RNGoogleSignin.h>
 #import <FBSDKCoreKit/FBSDKCoreKit.h>
 
+// #if DEBUG
+//   #ifdef FB_SONARKIT_ENABLED
+//   #import <FlipperKit/FlipperClient.h>
+//   #import <FlipperKitLayoutPlugin/FlipperKitLayoutPlugin.h>
+//   #import <FlipperKitLayoutPlugin/SKDescriptorMapper.h>
+//   #import <FlipperKitNetworkPlugin/FlipperKitNetworkPlugin.h>
+//   #import <FlipperKitReactPlugin/FlipperKitReactPlugin.h>
+//   #import <FlipperKitUserDefaultsPlugin/FKUserDefaultsPlugin.h>
+//   #import <SKIOSNetworkPlugin/SKIOSNetworkAdapter.h>
+//   #endif
+// #endif
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  // [self initializeFlipper:application];
+
   [FIRApp configure];
   
   [[FBSDKApplicationDelegate sharedInstance] application:application
@@ -54,6 +68,21 @@
                                                 openURL:url
                                                 options:options];
   }
+
+// Initialize flipper integrations
+// - (void) initializeFlipper:(UIApplication *)application {
+//   #if DEBUG
+//     #ifdef FB_SONARKIT_ENABLED
+//       FlipperClient *client = [FlipperClient sharedClient];
+//       SKDescriptorMapper *layoutDescriptorMapper = [[SKDescriptorMapper alloc] initWithDefaults];
+//       [client addPlugin: [[FlipperKitLayoutPlugin alloc] initWithRootNode: application withDescriptorMapper: layoutDescriptorMapper]];
+//       [client addPlugin: [[FKUserDefaultsPlugin alloc] initWithSuiteName:nil]];
+//       [client addPlugin: [FlipperKitReactPlugin new]];
+//       [client addPlugin: [[FlipperKitNetworkPlugin alloc] initWithNetworkAdapter:[SKIOSNetworkAdapter new]]];
+//       [client start];
+//     #endif
+//   #endif
+// }
 
 // Registers user notifications
 - (void)application:(UIApplication *)application didRegisterUserNotificationSettings:(UIUserNotificationSettings *)notificationSettings {
