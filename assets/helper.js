0d82234e432efea2798ae6b0665443d85322e821
Eric Furspan <eric.furspan@gmail.com>
2020-10-11 19:56:14 -0400

    Update assets

--- assets/helper.js
+++
@@ -1,9 +0,0 @@
-// helper.js - helper functions
-
-
-// sort array by date descending
-export let sortByDateDesc = (a,b) => {
-  let c = new Date(a.time);
-  let d = new Date(b.time);
-  return d-c;
-}
