262ee43519e13d5947735166efbdde321f72442a
Eric Furspan <eric.furspan@gmail.com>
2020-10-11 19:55:54 -0400

    Update config files

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,23 +11,19 @@ module.exports = {
     jest: true,
     'react-native/react-native': true,
   },
-  plugins: [
-    'react',
-    'react-native',
-  ],
+  plugins: ['react', 'react-native'],
   extends: [
     'airbnb',
     '@react-native-community',
     'prettier',
     'prettier/react',
     'plugin:prettier/recommended',
-    'plugin:react-hooks/recommended',
-    'eslint-config-prettier'
   ],
   rules: {
     'prettier/prettier': 'warn',
     'react/prop-types': 0,
     'react/no-array-index-key': 'warn',
+    'import/prefer-default-export': 0,
   },
   settings: {
     react: {
--- react-native.config.js
+++ react-native.config.js
@@ -1,4 +1,3 @@
 module.exports = {
   assets: ['./assets/'],
 };
-
